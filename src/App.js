7d09024fdcf3d42034b22ec1467fd80136d488cf
Adrian Werimo <adrwer7@gmail.com>
2021-05-25 17:37:48 +0300

    Added filter functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 //importing components
@@ -8,14 +8,35 @@ import TodoList from "./components/TodoList";
 function App() {
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setfilteredTodos] = useState([]);
+
+  useEffect(() => {
+    filterHandler();
+  }, [todos, status])
+
+
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setfilteredTodos(todos.filter(todo => todo.completed === true))
+        break;
+      case "uncompleted":
+        setfilteredTodos(todos.filter(todo => todo.completed === false))
+        break;
+      default:
+        setfilteredTodos(todos)
+        break;
+    }
+  }
 
   return (
     <div className="App">
       <header>
         <h1>Adrian's ToDo List</h1>
       </header>
-      <Form inputText={inputText} setInputText={setInputText} todos={todos} setTodos={setTodos}/>
-      <TodoList todos={todos} setTodos={setTodos} />
+      <Form inputText={inputText} setInputText={setInputText} todos={todos} setTodos={setTodos} setStatus={setStatus} />
+      <TodoList todos={todos} setTodos={setTodos} filteredTodos={filteredTodos} />
     </div>
   );
 }
